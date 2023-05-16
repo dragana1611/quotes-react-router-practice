cd306ae9843d09ef5867cd23d4e618ff8562f271
Dragana Petrovic <dragana.popov-petrovic@badin.rs>
2023-05-16 16:03:57 +0200

    implement lazy loading

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,4 +1,4 @@
-import { useHistory, useLocation, useRouteMatch } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 
 import QuoteItem from "./QuoteItem";
 import classes from "./QuoteList.module.css";
